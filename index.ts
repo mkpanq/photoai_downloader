306b389234b509abc3b26ff4f961f7d4edfe98f7
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-08-28 16:25:46 +0200

    Amazing finish, fix import

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { downloadPhotos, savePhotosMetadataToFile } from "./src/saving";
 import downloadPhotosMetadata from "./src/fetchData";
+import { init } from "./src/config";
 
 const main = async () => {
   init();
